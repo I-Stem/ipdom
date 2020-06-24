a9802cf5ef84be159218f439da9df5dd3f5cf6fc
ank3r <mwangishadrack5@gmail.com>
2020-06-24 06:22:12 +0300

    add extraction api

--- src/dom/find/mod.rs
+++ src/dom/find/mod.rs
@@ -1,6 +1,5 @@
-use super::{IpDom, Node};
+use super::{IpDom, Node, AttributeTypes};
 mod predicate;
-
 pub use predicate::Predicate;
 
 ///! Find iterator interface
@@ -8,6 +7,34 @@ pub struct Find<'a> {
     pub dom: &'a IpDom,
     pub predicate: Box<dyn Predicate>,
     pub next: usize,
+}
+
+impl <'a> Find <'a>{
+    pub fn extract(&mut self) -> Vec<String>{
+        let mut v = Vec::new();
+
+        while self.next < self.dom.len() {
+            let node = self.dom.nth(self.next).unwrap();
+
+            self.next += 1;
+            
+            if self.predicate.macthes(&node){
+                let data = node.data().read().unwrap();
+
+                //extract the attribute 
+                let attr = data.attrs();
+                if let Some(q) = attr.get("question"){
+                    if let AttributeTypes::Str(s) = q {
+                        v.push(s.clone());
+                    }
+                   
+                }
+
+            }
+        }
+
+        v
+    }
 }
 
 
